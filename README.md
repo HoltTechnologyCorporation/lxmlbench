556452d415b838b745234f732b808024bf26c3ed
Gregory Petukhov <lorien@lorien.name>
2019-12-01 23:00:46 +0300

    Update

---
+++ README.md
@@ -0,0 +1,11 @@
+# CPU Speed Benchmark
+
+This is simple benchmark which uses lxml library to parse HTML.
+
+## Installation
+
+You need python3 and lxml library. You can install lxml library with command: `python3 -m pip install lxml`
+
+## How to run test
+
+Just run command `pyton3 runtest.py`
--- runtest.py
+++ runtest.py
@@ -2,7 +2,9 @@
 from argparse import ArgumentParser
 import time
 from multiprocessing import cpu_count, Process, Queue
+from urllib.request import urlopen
 from queue import Empty
+import os
 
 from lxml.html import fromstring
 
@@ -29,38 +31,49 @@ def thread_parser(data, taskq):
             assert 'reddit' in dom.xpath('//title')[0].text
 
 
+def download_file(url, path):
+    if not os.path.exists(path):
+        print('Downloading %s to %s' % (url, path))
+        with open(path, 'wb') as out:
+            out.write(urlopen(url).read())
+
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('-n', '--tasks-number', type=int, default=10)
     opts = parser.parse_args()
     total_num_cpu = cpu_count()
-    with open('data/reddit.html') as inp:
+    download_file(
+        'https://raw.githubusercontent.com'
+        '/lorien/lxmlbench/master/data/reddit.html',
+        '.reddit.html'
+    )
+    with open('.reddit.html') as inp:
         data = inp.read()
-
-    taskq = Queue()
-    for _ in range(opts.tasks_number):
-        taskq.put(None)
 
     num_cpu_used = set()
     history = []
-    #for div in (None, 0.25, 5, 0.75, 1):
-    for div in [None]:
+    for div in (None, 0.25, 0.5, 0.75, 1):
+        taskq = Queue()
+        for _ in range(opts.tasks_number):
+            taskq.put(None)
+
         if div is None:
             num_cpu = 1
         else:
             num_cpu = round(total_num_cpu * div)
         if num_cpu not in num_cpu_used:
             num_cpu_used.add(num_cpu)
-            print('Using %d CPUs' % num_cpu, end='')
+            print('Using %d CPUs' % num_cpu, end=': ')
             started = time.time()
             pool = []
-            for _ in range(2):
+            for _ in range(num_cpu):
                 proc = Process(target=thread_parser, args=[data, taskq])
                 proc.start()
                 pool.append(proc)
             [x.join() for x in pool]
             elapsed = time.time() - started
-            print('%.2f' % elapsed)
+            print('%.2f sec' % elapsed)
             history.append((num_cpu, elapsed))
     model_name = parse_cpu_info('model name')
     cache_size = parse_cpu_info('cache size')
